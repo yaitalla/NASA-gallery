e1486294aeaa4e140989bea7a8bcd2eaa45a2623
yaitalla <yaitalla.dev@gmail.com>
2021-02-02 12:47:37 +0100

    image preview

---
+++ src/components/ImgPreview/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import {PreWrap} from './style';
+
+const ImagePreview = ({imgUrl, nasaId}) => {
+    return (
+        <PreWrap>
+            
+        </PreWrap>
+    )
+}
+
+export default ImagePreview;
